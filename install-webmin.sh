cb5ec29553571e56cb39e4074a2476ba25d59051
Andre Rampersad <andre.raden+github@gmail.com>
2019-03-24 03:29:54 -0400

    Update webmin installation script
    Tailored for CentOS minimal

--- install-webmin.sh
+++ install-webmin.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-touch /etc/apt/sources.list.d/webmin.list
-echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list.d/webmin.list
-echo "http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list.d/webmin.list
+yum -y update
+yum -y install wget
+wget http://prdownloads.sourceforge.net/webadmin/webmin-1.900-1.noarch.rpm
+yum -y install perl perl-Net-SSLeay openssl perl-IO-Tty
+rpm -U webmin-1.900-1.noarch.rpm
+
+firewall-cmd --zone=public --add-port=10000/tcp --permanent
+firewall-cmd --reload
+
+service webmin start
+chkconfig webmin on
